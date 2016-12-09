d47019cf0e36d9f6aa11dfa974c12c873c0f4b40
bonattt <bonattt@rose-hulman.edu>
2016-12-09 17:16:08 -0500

    defined package and command, and added an (unhelpful) default help function

--- project/ex_env/__init__.py
+++ project/ex_env/__init__.py
@@ -1,16 +1,55 @@
 
 
-class ExecutionEnv():
+class Package():
 
-    def __inti__(self, fields={}, commands={}):
+    def __init__(self, name, fields={}, commands={}):
         self.fields = fields
         self.commands = commands
+        self.name = name
 
-    def run(self, path_name):
+    def run(self):
         while True:
-            user_in = input(path_name + ' >> ')
+            user_in = input(self.name + ' >> ')
             if user_in == '':
                 continue
-
             user_in = user_in.split(' ')
             command = user_in[0]
+            args = user_in[1:]
+            if '.' in command:
+                print('TODO handle sub-package commands')
+            elif command == 'help':
+                self.commands[args[0]].help(args[1:], self.fields)
+            elif not self.contains_command(command):
+                print('command "' + command + "' not recognized")
+            else:
+                exe_cmd = self.commands[command]
+                exe_cmd.execute(args, self.fields)
+
+    def contains_command(self, command):
+        return command in self.commands
+
+    def execute(self, args, fields):
+        self.run()
+
+    def add_command(self, command, name):
+        if name in self.commands:
+            print("that command alread exists in package", self.name)
+        else:
+            self.commands[name] = command
+
+    def is_package(self):
+        return True
+
+
+def default_help(args, fields):
+    print("no help function is defined for this function")
+
+
+class Command():
+
+    def __init__(self, func, help=default_help):
+        self.execute = func
+        self.help = help
+
+    def is_package(self):
+        return False
--- project/hermitCrab.py
+++ project/hermitCrab.py
@@ -1,9 +1,31 @@
-
+from ex_env import Package, Command
 
 def run():
-    print("run")
+    env = Package("Hermit-Crab")
+    env.add_command(Command(echo), "echo")
+    env.add_command(Command(q), "q")
+    env.add_command(Command(q), "quit")
+    env.add_command(Command(q), "exit")
+    env.run()
 
+def echo(args, fields):
+    for item in args:
+        print(item)
 
+def q(args, fields):
+    if not '--yes' in args:
+        user_in = input("are you sure you would like to quit? (yes/no) ")
+        if not user_in.startswith('y'):
+            return
+    exit(0);
+
+def plus(args, fields):
+    try:
+        accumulator = 0
+        for numb in args:
+            accumulator += int(numb)
+    except Exception as a:
+        print(a)
 
 
 if __name__ == "__main__":
